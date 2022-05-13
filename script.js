c104889cca42340166fb08ec8fc77a1f69852f96
Nadirathoha <102664938+Nadirathoha@users.noreply.github.com>
2022-05-13 17:45:53 +0530

    new git

---
+++ script.js
@@ -0,0 +1,97 @@
+function validate(callback) {
+
+    let username=  document.getElementById("username");
+    let password=  document.getElementById("password");
+
+    if(username.value=="admin" && password.value=="12345"){
+        callback();
+        }
+        else{ 
+            alert("Invalid user")   
+            
+            }
+}
+
+function goto(){
+    window.location.href="main.html";
+
+
+}
+  function displaylist(){
+     var httpRequest=  new XMLHttpRequest();
+     httpRequest.onreadystatechange= function(){
+         try{
+             if (httpRequest.readyState===XMLHttpRequest.DONE ) {
+                 if(httpRequest.status===200){
+                    console.log(httpRequest.responseText);
+                    display(httpRequest.responseText);
+             }
+             else{
+                 alert("Error");
+             }
+         }
+        }
+         catch(e){
+             alert(e.description);
+         }
+     }
+     httpRequest.open("GET","https://jsonplaceholder.typicode.com/todos",true);
+     httpRequest.send();
+    }
+
+
+
+
+
+function logout() {
+    location.replace("index.html"); 
+}
+
+
+ function display(data) {
+     var list=JSON.parse(data);
+     let table=document.getElementById("result");
+      for(var i=0;i<list.lenght;i++) {
+          let rowcount=table.rows.length;
+          var row=table.insertRow(rowcount);
+          var cell1=row.insertCell(0);
+          cell1.innerHTML=list[i].id; 
+
+          var cell2=row.insertCell(1);
+          cell2.innerHTML=list[i].title;
+          
+          var cell3=row.insertCell(2);
+          var element=document.createElement("input");
+          element.type="checkbox";
+
+
+          if (list[i].compleated==true) {
+              element.setAttribute("checked","true");
+              element.setAttribute("disabled","true");
+            }
+            element.addEventListener('change',(event)=>{
+                if (event.currentTarget.checked) {
+                    count++;
+                    checkCounter();
+                }
+                else{
+                    count--;
+                }
+            })
+            cell3.appendChild(element);
+            
+      }
+ }
+
+
+var count=0;
+function checkCounter(){
+    let promise=new promise(function(resolve,reject){
+        if (count==5) {
+            resolve("Congrats. 5 Tasks have been Successfully Completed");
+        }
+    })
+    promise.then(function(s){
+        alert(s);
+    })
+}
